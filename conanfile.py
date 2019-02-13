eea9396a39cf02b3c6280e779bc82f570b9fd412
SSE4 <tomskside@gmail.com>
2019-02-13 13:11:41 +0700

    - always use our bison to ensure version is up-to-date
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -40,10 +40,8 @@ class GStreamerConan(ConanFile):
     def build_requirements(self):
         if not tools.which("meson"):
             self.build_requires("meson_installer/0.49.0@bincrafters/stable")
-        if not tools.which("bison"):
-            self.build_requires("bison/3.0.5@bincrafters/stable")
-        if not tools.which("flex"):
-            self.build_requires("flex/2.6.4@bincrafters/stable")
+        self.build_requires("bison/3.0.5@bincrafters/stable")
+        self.build_requires("flex/2.6.4@bincrafters/stable")
 
     def source(self):
         self.run("git clone https://gitlab.freedesktop.org/gstreamer/gstreamer.git --branch %s --depth 1" % self.version)
