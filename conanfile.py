c670312490d321c0252a8377846388d2854f76fd
bincrafters-user <bincrafters@gmail.com>
2020-04-24 04:44:28 +0000

    Update Conan conventions
    Automatically created by bincrafters-conventions 0.24.0

--- conanfile.py
+++ conanfile.py
@@ -35,7 +35,7 @@ class GStreamerConan(ConanFile):
             del self.options.fPIC
 
     def build_requirements(self):
-        self.build_requires("meson/0.53.2")
+        self.build_requires("meson/0.54.0")
         if not tools.which("pkg-config"):
             self.build_requires("pkg-config_installer/0.29.2@bincrafters/stable")
         self.build_requires("bison_installer/3.3.2@bincrafters/stable")
